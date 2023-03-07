3e99d5158a21ebb4b96f8127678fff69697a6c1e
lee <doch02@naver.com>
2023-03-07 12:46:46 +0900

    그리디 완료

--- 이코테_문제풀이/이수은/그리디/무지의 먹방 라이브.py
+++ 이코테_문제풀이/이수은/그리디/무지의 먹방 라이브.py
@@ -1,29 +1,30 @@
 import heapq
+import sys
+input = sys.stdin.readline
 
 def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
+    length = len(food_times)
+    q = []  # 음식시간, 음식번호 순
+    for i in range(length):
+        heapq.heappush(q, (food_times[i],i+1))
+
+    sum_time, prev_time = 0, 0
+    # sum_time : 여태까지 먹은 음식 시간 합
+    # prev_time : 이전에 먹은 음식 시간
+    while sum_time + ((q[0][0] - prev_time) * length) <= k:
+        now_time = heapq.heappop(q)[0]
+        sum_time += (now_time - prev_time) * length
+        prev_time = now_time
+        length -= 1
     
-  # 전체 음식을 다 먹는 시간이 k보다 작거나 같으면 모든 음식을 다 먹었으므로 -1반환
-  if sum(food_times)<=k:
-    return -1
-  
-  # 우선순위큐(최소힙)에다가 삽입
-  # 음식 번호와 시간을 튜플형태로 삽입, 시간을 기준으로 최소힙 완성됨
-  q=[]
-  for i in range(len(food_times)):
-    heapq.heappush(q, (food_times[i],i+1))
-  
-  
-  foods = len(food_times) # 남은 음식의 개수
-  previous = 0 # 이전 음식을 먹는데 걸리는 시간
-  while True:
-    # 남은 음식의 개수 * 음식을 먹는 시간 = 이 음식을 다 먹을 때까지 걸리는 시간
-    now = q[0][0] # 여기서 pop을 하면 q에 아무것도 안남아서 마지막에 정렬이 안됨
-    t=foods*(now-previous) # 한 음식을 다 먹어서 빼면 튜플에 남아있는 음식을 먹기위해 걸리는 시간이 줄어들어야함
-    if k>=t:
-      k-=t
-      foods-=1
-      previous = heapq.heappop(q)[0] # now와 같은 값이지만 여기서 pop을 함
-    else:
-      i = k%foods
-      q.sort(key=lambda x:x[1]) # 음식 번호로 정렬
-      return q[i][1]
+    # 우선순위큐 -> 음식번호순 
+    result = sorted(q, key = lambda x:x[1])
+    return result[(k - sum_time) % length][1]
+
+food_times = list(map(int, input().split()))
+k = int(input())
+
+print(solution(food_times,k))
